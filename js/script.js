0aec4b3f9c6b6b11dad247e617b223299fafa3c0
choyon das <choyondas08@gmail.com>
2021-08-21 03:15:14 +0800

    price section done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/* 
+create a function to get the value of memory, storage etc 
+*/
+
 function memory(extraConfiguration, memoryCapacity) {
     const memory = document.getElementById(extraConfiguration);
     const memoryCost = memory.innerText;
@@ -18,7 +22,25 @@ function memory(extraConfiguration, memoryCapacity) {
         memory.innerText = 20;
     }
 
-}
+    const bestPrice = document.getElementById('bestPrice').innerText;
+    const ExtraMemoryCost = document.getElementById('ExtraMemoryCost').innerText;
+    const ExtraStorageCost = document.getElementById('ExtraStorageCost').innerText;
+    const extraDeliveryCharge = document.getElementById('extraDeliveryCharge').innerText;
+
+    const Price = document.getElementById('totalPrice');
+    Price.innerText = parseInt(bestPrice) + parseInt(ExtraMemoryCost) + parseInt(ExtraStorageCost) + parseInt(extraDeliveryCharge);
+
+
+
+
+};
+
+
+
+
+
+
+
 /* 
 make memory section interactive
  */
@@ -36,17 +58,17 @@ make storage section interactive
  */
 document.getElementById('storage256').addEventListener('click', function() {
     memory('ExtraStorageCost', '256');
-})
+});
 document.getElementById('storage512').addEventListener('click', function() {
     memory('ExtraStorageCost', '512');
-})
+});
 document.getElementById('storage1TB').addEventListener('click', function() {
     memory('ExtraStorageCost', '1TB');
-})
+});
 
 document.getElementById('freeDelivery').addEventListener('click', function() {
     memory('extraDeliveryCharge', 'free')
-})
+});
 document.getElementById('DeliveryCharge').addEventListener('click', function() {
     memory('extraDeliveryCharge', 'notFree')
-})
+});
